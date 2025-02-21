12ae87bcfec5f963da60d371e84c22c5a765ef42
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-02-21 23:49:19 +0900

    feat : 예외와 로그

--- shorten-url-for-logging/src/main/java/kr/co/shortenurlservice/domain/NotFoundShortenUrlException.java
+++ shorten-url-for-logging/src/main/java/kr/co/shortenurlservice/domain/NotFoundShortenUrlException.java
@@ -1,4 +1,8 @@
 package kr.co.shortenurlservice.domain;
 
 public class NotFoundShortenUrlException extends RuntimeException {
+
+    public NotFoundShortenUrlException(String message) {
+        super(message);
+    }
 }
